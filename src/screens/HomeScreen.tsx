332d12132924d1b0ef6a8c3cca86104a3d9c2338
Bohdan Lobor <b.lobor@hey.com>
2020-12-26 22:37:13 +0200

    fix: icon centering

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 
 import './HomeScreen.css'
 
+import { PlusIcon } from "../components/PlusIcon";
+
 export const HomeScreen = (): ReactElement => {
   return (
-   <main className='main_container'>
+   <main className='main__container'>
      <header className='main__header'>
        <Link className='main__link' to='/'>
          <h1>Wallet</h1>
        </Link>
-       <Link className='main__link main__scan-link' to='/scan'>+</Link>
+       <Link className='main__link main__scan-link' to='/scan'>
+         <PlusIcon />
+       </Link>
      </header>
      <section>
        <ul>
